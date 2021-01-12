6c7bcc5c4dd4b2a1d53af2e7c4e8f79f1a04aac2
Machalka <al1999dk@gmail.com>
2021-01-12 13:12:46 +0300

    мигрировал модели Task и Solution

--- expert/models.py
+++ expert/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from team.models import Teams
 
-# Create your models here.
+
+class Task(models.Model):
+    title=models.CharField(max_length=100, blank=False, unique=True, verbose_name="Заголовок")
+    task=models.TextField(blank=False, verbose_name="Задание")
+    task_file=models.FileField(blank=True, null=True, verbose_name="Файл задания")
+    cost=models.PositiveIntegerField(blank=False, null=False, default=0, verbose_name="Максимальный балл за задание")
+    deadline=models.DateTimeField(blank=False, null=False, auto_now_add=True, verbose_name="Дедлайн")
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+class Solution(models.Model):
+    team=models.ForeignKey(Teams, on_delete=models.SET_NULL, null=True, blank=False, verbose_name="Команда")
+    task=models.ForeignKey(Task, on_delete=models.SET_NULL, null=True, blank=False, verbose_name="Задача")
+    solution=models.TextField(blank=False,verbose_name="Решение")
+    solution_file=models.FileField(blank=True, null=True, verbose_name="Файл решения")
+    score=models.PositiveIntegerField(blank=True, null=True, verbose_name="Баллы за задание")
+    def __str__(self):
+        return self.task
+    class Meta:
+        verbose_name = 'Решение'
+        verbose_name_plural = 'Решения'
