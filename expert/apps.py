d317e83b0d2b124a68ebb856a8123e1e1e55e7fe
Machalka <al1999dk@gmail.com>
2021-01-12 16:24:50 +0300

    изменил apps.py

--- expert/apps.py
+++ expert/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class ExpertConfig(AppConfig):
     name = 'expert'
+    verbose_name='Эксперты'
--- userAuth/apps.py
+++ userAuth/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class UserauthConfig(AppConfig):
     name = 'userAuth'
+    verbose_name='Пользователи'
