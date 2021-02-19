965e48d8debecb31df82141a9199eca38d35ea93
Machalka <al1999dk@gmail.com>
2021-02-19 23:56:53 +0300

    переложил сортировку групп на фронт

--- team/static/team/get_teams.js
+++ team/static/team/get_teams.js
@@ -1,10 +1,12 @@
 let m = document.getElementById("main");
+var test;
 function createTeams(teams){
     let cards="";
     if(teams.length==0){
         document.getElementById("teams").innerHTML="Нет команд";
         m.innerHTML='<img src="'+img_empty_street+'" alt="пусто..." style="width: 60%; margin-top: 3%; margin-bottom: 2%;">\n<p class="lead">Ни одна команда не зарегистрировалась 😢</p>';
     }else{
+        teams=teams.sort((a,b)=>b['score']-a['score']);
         for(let i=0; i<teams.length; i++){
             cards+='<a class="a-card" href="/team/'+teams[i]["name"]+'">\n<div class="card bg-light mb-4">\n<div class="card-header"><h5>'+teams[i]["name"]+'</h5></div>\n<div class="card-body">\n<p>Счет '+teams[i]['score']+'</p>\n</div>\n</div>\n</a>\n';
         }
@@ -19,6 +21,8 @@ function getTeams() {
         dataType: 'json',
         data: send,
         success: function (data) {
+            test=data["teams"];
+            console.log(data["teams"]);
             createTeams(data["teams"]);
         },
         error: function () {
