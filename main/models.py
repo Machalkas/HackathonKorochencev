8d4131f649a9646ad9dcef011723be987a3017ac
Machalka <al1999dk@gmail.com>
2021-02-23 00:04:01 +0300

    добавил end_date в Settings

--- main/models.py
+++ main/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Settings(models.Model):
-    start_date=models.DateTimeField(verbose_name="Дата начала")
-    max_teams=models.PositiveIntegerField(verbose_name="Максимальное количество команд")
-    max_members=models.PositiveIntegerField(verbose_name="Максимальное количество участников в команде")
+    start_date=models.DateTimeField(blank=True, null=True, verbose_name="Дата начала")
+    end_date=models.DateTimeField(blank=True, null=True, verbose_name="Дата окончания")
+    max_teams=models.PositiveIntegerField(blank=True, null=True, verbose_name="Максимальное количество команд")
+    max_members=models.PositiveIntegerField(blank=True, null=True, verbose_name="Максимальное количество участников в команде")
     class Meta:
         verbose_name = 'Параметры'
         verbose_name_plural = 'Параметры'
